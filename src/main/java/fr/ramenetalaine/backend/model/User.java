08f0c6e872e29426cadb21d21b7c22192c83fce5
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-02 11:06:14 +0200

    feat: improve user entity with email uniqueness

--- src/main/java/fr/ramenetalaine/backend/model/User.java
+++ src/main/java/fr/ramenetalaine/backend/model/User.java
@@ -1,15 +1,22 @@
 package fr.ramenetalaine.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "app_user")
-@Data
+@Table(
+    name = "app_user",
+    uniqueConstraints = @UniqueConstraint(name = "uk_app_user_email", columnNames = "email")
+)
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "password")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -19,11 +26,15 @@ public class User {
     private String nom;
     @Column(unique = true, nullable = false)
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
     private String password;
     private String surnom;
     private String ville;
     private String img;
 
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
