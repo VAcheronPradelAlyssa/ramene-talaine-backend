feb71ce1f05e9cf71ac46a31285c562e8ccf6f59
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-02 11:38:42 +0200

    feat: add endpoint to get current user profile

---
+++ src/main/java/fr/ramenetalaine/backend/dto/CurrentUserDto.java
@@ -0,0 +1,13 @@
+package fr.ramenetalaine.backend.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CurrentUserDto {
+    private String id;
+    private String email;
+    private LocalDateTime createdAt;
+}
